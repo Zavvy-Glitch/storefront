90eadef1cb7e0c9e87188d16862992be3f2d6724
Chattray <chattray.chea@gmail.com>
2022-05-09 21:29:39 -0700

    redux functions update

--- storefront/src/store/Products.js
+++ storefront/src/store/Products.js
@@ -2,52 +2,54 @@ const initialState = {
   products: [
     { 
       id: 1,
-      category: 'Electronics', 
+      category: 'electronics', 
       name:'Computer' , 
       description: 'NEWLY USED / 2016 HP', 
       price:'FREE', 
-      inventoryCount: '1',
+      inventoryCount: 100,
     }, 
     {
       id: 2,
-      category: 'Food',
+      category: 'food',
       name: 'Banana',
       description: 'It\'s yellow',
       price:'FREE',
-      inventoryCount: '1',
+      inventoryCount: 50,
     }
   ],
+  filterProduct: []
 }
 
 function productsReducer (state = initialState, action) {
 
   switch (action.type) {
-    case 'PRODUCTS':
+    case 'UPDATEACTIVE':
       return {
         ...state,
-        products: state.products.map(product => {
-          if(product.category === action.payload.category) {
-            return {
-              name: product.name,
-              description: product.description,
-              price: product.price,
-              inventoryCount: product.inventoryCount
-            }
-          }
-          return product;
-        }),
+        filterProduct: state.products.filter(product => product.category === action.payload.name)
       }
       case 'RESET':
-        return initialState;
+        return {
+          ...state,
+          filterProduct: []
+        }
       default:
         return state;
+    } 
+  }
+
+
+
+export const updateList = (category) => {
+  return {
+    type: 'UDPATEACTIVE',
+    payload: category
   }
 }
 
-export const productItem = (product) => {
-  return {
-    type: 'PRODUCTS',
-    payload: product
+export const reset = () => {
+  return{
+    type: "RESET"
   }
 }
 
