3d63c520ece43dcec0a609b8209103e522236f01
Chattray <chattray.chea@gmail.com>
2022-05-10 19:36:01 -0700

    adds cart list functionality - needs update on logic to not delete multiples of same item

---
+++ src/component/cart/CartList.js
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from "react-redux";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { deleteFromCart } from "./Cart.js"
+
+export default function CartList() {
+  let cart = useSelector((state) => state.cart.cartItems);
+  let cartCount = useSelector((state) => state.cart.cartCount);
+  let dispatch = useDispatch();
+
+  const handleDelete = (item) => {
+    let action = deleteFromCart(item);
+    dispatch(action)
+  }
+
+  console.log(cart);
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {cart.length
+        ? cart.map((item) => (
+            <Card key={item.id}>
+              <CardContent>
+                <Typography>{item}</Typography>
+                <button onClick={() => handleDelete(item)}>Remove</button>
+              </CardContent>
+            </Card>
+          ))
+        : null}
+      <Typography>Cart Items:{cartCount}</Typography>
+    </div>
+  );
+}
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 import categoryReducer from '../component/categories/Categories';
-import productsReducer from '../component/products/Products'
+import productsReducer from '../component/products/Products';
+import cartReducer from '../component/cart/Cart'
 
 let reducers = combineReducers({
   categories: categoryReducer,
   products: productsReducer,
+  cart: cartReducer,
 });
 
 export default function initialStore() {
