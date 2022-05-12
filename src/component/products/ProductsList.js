861c4d780fe26af51751057b2e421a5129363670
Chattray <chattray.chea@gmail.com>
2022-05-12 16:16:16 -0700

    filtering w/ increment & decrement inventory on products

--- src/component/products/ProductsList.js
+++ src/component/products/ProductsList.js
@@ -4,34 +4,39 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { addToCart } from "../../store/Cart";
+import { getProduct } from "../../store/Products";
 import { CardActions, Button } from "@mui/material";
+import { useEffect } from "react";
 
 export default function ProductList() {
+
   let products = useSelector((state) => state.products);
+
+  const handleCart = (item) => {
+    let cartAction = addToCart(item);
+    dispatch(cartAction);
+  };
+
+  console.log(products);
 
   let dispatch = useDispatch();
 
-  const handleCart = (item) => {
-    let action = addToCart(item);
-    dispatch(action);
-  };
-
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
 
   return (
     <Box>
-      {products.products.length
+      {products.filterProduct.length
         ? products.filterProduct.map((product) => (
-            <Card key={product.id}>
+            <Card key={product._id}>
               <CardContent>
                 <Typography>{product.name}</Typography>
-                <Typography>{product.description}</Typography>
                 <Typography>Price: {product.price}</Typography>
-                <Typography>Inventory: {product.inventoryCount}</Typography>
+                <Typography>Inventory: {product.inStock}</Typography>
               </CardContent>
               <CardActions>
-                <Button onClick={() => handleCart(product.name)}>
-                  Add To Cart
-                </Button>
+                <Button onClick={() => handleCart(product)}>Add To Cart</Button>
               </CardActions>
             </Card>
           ))
